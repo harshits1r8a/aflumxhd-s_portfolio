666152eab21de5c7d69dff9945a26f52f0a7207f
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2024-04-28 23:31:02 +0530

    done for desktop screen

--- src/DB/Testimonialdata.js
+++ src/DB/Testimonialdata.js
@@ -1,6 +1,6 @@
 const Testimonialdata = [
     {
-        img: "/img/pp.jpg",
+        img: "/img/p1.jpg",
         name: "Gautam Sukhramani",
         pos: "CTO, Intellection",
         description: {
@@ -15,7 +15,7 @@ const Testimonialdata = [
         }
     },
     {
-        img: "/img/pp.jpg",
+        img: "/img/p2.jpg",
         name: "Shiyab K",
         pos: "Founder, Arabian Travels ",
         description: {
@@ -30,7 +30,7 @@ const Testimonialdata = [
         }
     },
     {
-        img: "/img/pp.jpg",
+        img: "/img/p3.jpg",
         name: "Rahul Agarwal",
         pos: "CEO &Product Manager, Intellection",
         description: {
@@ -45,7 +45,37 @@ const Testimonialdata = [
         }
     },
     {
-        img: "/img/pp.jpg",
+        img: "/img/p1.jpg",
+        name: "Harshir",
+        pos: "CEO &Product Manager, Intellection",
+        description: {
+            title: "It was a great experience",
+            star: 2,
+            message: `Aflaah This can be done in a multitude of ways for example if you are producing a brochure selling wellington boots then start every
+            paragraph spelling out 'buy our wellington boots' or better still 'you have no choice but to buy our wellington boots now get
+            out your cheque book and buy them - NOW!'
+            
+            Another crafty method is to doctor images in photoshop so that on an innocent picture of a tree for example carved into the bark is a message along the lines of 'U Luv Wellington
+            boots' You get the idea.`
+        }
+    },
+    {
+        img: "/img/p1.jpg",
+        name: "Harshir",
+        pos: "CEO &Product Manager, Intellection",
+        description: {
+            title: "It was a great experience",
+            star: 2,
+            message: `Aflaah This can be done in a multitude of ways for example if you are producing a brochure selling wellington boots then start every
+            paragraph spelling out 'buy our wellington boots' or better still 'you have no choice but to buy our wellington boots now get
+            out your cheque book and buy them - NOW!'
+            
+            Another crafty method is to doctor images in photoshop so that on an innocent picture of a tree for example carved into the bark is a message along the lines of 'U Luv Wellington
+            boots' You get the idea.`
+        }
+    },
+    {
+        img: "/img/p1.jpg",
         name: "Harshir",
         pos: "CEO &Product Manager, Intellection",
         description: {
--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -7,15 +7,28 @@ import './Scroll.css';
 
 const Testimonial = () => {
   const [testimonials, setTestimonials] = useState([]);
-  const [leftindex, setLeftIndex] = useState(0);
-  
+  const [leftindex, setLeftIndex] = useState(1);
 
   useEffect(() => {
     setTestimonials(Testimonialdata);
   }, []);
   console.log(leftindex);
 
-  
+  const panels = document.querySelectorAll('.panel');
+  // panels[0].classList.add('active')
+
+  panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+      removeActive();
+      panel.classList.add('active');
+    },true);
+  });
+
+  function removeActive() {
+    panels.forEach((panel) => {
+      panel.classList.remove('active');
+    });
+  }
 
   return (
     <div className="pl-[2.5rem] pr-[2.5rem]  relative h-[90vh]  w-screen  bg-background  overflow-hidden ">
@@ -28,9 +41,13 @@ const Testimonial = () => {
       <div className="h-[70vh] w-full flex ">
         {/* left side */}
         <div className=" h-[350px] z-30 w-[50%] flex justify-end items-start     ">
-          <div className=" h-full flex flex-col justify-start  overflow-y-scroll pr-10 scroll">
+          <div className=" h-full  flex flex-col justify-start  overflow-y-scroll pr-10 pl-10 scroll ">
             {testimonials.map((elm, index) => (
-              <div className=''  key={index} onClick={()=>setLeftIndex(index)}>
+              <div
+                className={`${index === 1 ? 'active' : ''} panel rounded-lg cursor-pointer  transition ease-in-out duration-300   hover:shadow-lg hover:scale-105 hover:bg-white mt-5`}
+                key={index}
+                onClick={() => setLeftIndex(index)}
+              >
                 <LeftTestimonial person={elm} />
               </div>
             ))}
@@ -39,10 +56,11 @@ const Testimonial = () => {
 
         {/* Right side */}
         <div className=" h-full  w-[50%]  flex justify-start  items-start pt-5  pl-14">
-          {testimonials.map((elm, index) =>
-            index === leftindex ? ( // Condition
-              <RightTestimonial description={elm.description} key={index} />
-            ) : null // Or another component/element if not positive
+          {testimonials.map(
+            (elm, index) =>
+              index === leftindex ? ( // Condition
+                <RightTestimonial description={elm.description} key={index} />
+              ) : null // Or another component/element if not positive
           )}
         </div>
       </div>
@@ -54,7 +72,7 @@ const Testimonial = () => {
         linearGradiant="bg-gradient-to-bl from-violet-500 to-slate-100"
         width="w-[500px]"
         height="h-[500px]"
-        top="top-[530px]"
+        top="top-[550px]"
         left="left-[510px]"
         opacity="opacity-40"
         z_index="z-0"
